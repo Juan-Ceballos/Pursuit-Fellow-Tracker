208e23d00121abc9c5a6785062ec366a538dba73
Juan C <juanceballos@pursuit.org>
2021-08-23 23:48:50 -0400

    tested network helper

--- CodeWarsTrackerTests/CodeWarsTrackerTests.swift
+++ CodeWarsTrackerTests/CodeWarsTrackerTests.swift
@@ -10,24 +10,25 @@ import XCTest
 
 class CodeWarsTrackerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testNetworkHelper() {
+        let exp = XCTestExpectation(description: "data fetched")
+        guard let url = URL(string: RequestURLString.base) else {
+            XCTFail("bad url")
+            return
         }
+        let urlRequest = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(request: urlRequest) { (result) in
+            switch result {
+            case .failure(let appError):
+                print(appError)
+                XCTFail("\(appError)")
+            case .success(let data):
+                exp.fulfill()
+                XCTAssertGreaterThan(data.count, 1_000, "data should be greater than \(data.count)")
+            }
+        }
+        
+        wait(for: [exp], timeout: 5.0)
     }
 
 }
