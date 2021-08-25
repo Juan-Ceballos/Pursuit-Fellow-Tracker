4e963a161a2df9fdded463f6fe53864c58c89c02
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2021-08-25 01:04:02 -0400

    fixed stackview

--- CodeWarsTracker/Views/ScorecardView.swift
+++ CodeWarsTracker/Views/ScorecardView.swift
@@ -18,19 +18,24 @@ class ScoreCardView: UIView {
     
     public lazy var scoreboardContainerStackView: UIStackView = {
         let stackview = UIStackView()
-        stackview.axis = .horizontal
+        stackview.axis = .vertical
         stackview.spacing = 8
+        stackview.distribution = .equalSpacing
+        stackview.alignment = .center
         stackview.backgroundColor = .systemBlue
         stackview.addArrangedSubview(scoreboardLabel)
         stackview.addArrangedSubview(scoreboardDataStackView)
+        stackview.layoutMargins = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        stackview.isLayoutMarginsRelativeArrangement = true
         return stackview
     }()
     
     public lazy var scoreboardDataStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 20
+        stackView.axis = .horizontal
+        stackView.spacing = 8
         stackView.alignment = .center
+        stackView.distribution = .equalSpacing
         stackView.backgroundColor = .systemBlue
         stackView.addArrangedSubview(scoreboardWeeklyLabel)
         stackView.addArrangedSubview(scoreboardMonthlyLabel)
@@ -41,7 +46,9 @@ class ScoreCardView: UIView {
     public lazy var scoreboardLabel: UILabel = {
         let label = UILabel()
         label.text = "Score Board"
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
         label.textColor = .systemBackground
+        label.backgroundColor = .systemBlue
         return label
     }()
     
@@ -50,6 +57,7 @@ class ScoreCardView: UIView {
         label.textColor = .systemBackground
         label.numberOfLines = 3
         label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.text = "Weekly\nFellows:\nStaff:"
         return label
     }()
     
@@ -58,6 +66,7 @@ class ScoreCardView: UIView {
         label.textColor = .systemBackground
         label.numberOfLines = 3
         label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.text = "Monthly\nFellows:\nStaff:"
         return label
     }()
     
@@ -66,6 +75,7 @@ class ScoreCardView: UIView {
         label.textColor = .systemBackground
         label.numberOfLines = 3
         label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.text = "All Time\nFellows:\nStaff:"
         return label
     }()
     
@@ -87,8 +97,9 @@ class ScoreCardView: UIView {
         addSubview(scoreboardContainerStackView)
         scoreboardContainerStackView.snp.makeConstraints { mkr in
             mkr.centerX.equalToSuperview()
-            mkr.edges.equalTo(self).offset(8)
-            mkr.size.height.lessThanOrEqualTo(self).multipliedBy(0.2)
+            mkr.top.equalTo(safeAreaLayoutGuide).offset(10)
+            //mkr.edges.equalTo(self).offset(8)
+            //mkr.size.height.lessThanOrEqualTo(self).multipliedBy(0.5)
         }
     }
 }
